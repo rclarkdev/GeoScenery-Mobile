7d288c93c7f4aa06eb8615c1e60e28d03844be6d
ryan <rclarkdev1980@gmail.com>
2019-09-24 20:48:30 -0700

    observe page for scenery

---
+++ src/app/scenery/observe/observe.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-observe',
+  templateUrl: './observe.page.html',
+  styleUrls: ['./observe.page.scss'],
+})
+export class ObservePage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
