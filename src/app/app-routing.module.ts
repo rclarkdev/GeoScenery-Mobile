c6675737a5e3a0c83220eea0d91897d86f70386f
ryan <rclarkdev1980@gmail.com>
2019-09-24 20:57:22 -0700

    adjust routes for top level, and default to scenery

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,17 +2,10 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+  { path: '', redirectTo: 'scenery', pathMatch: 'full' },
   { path: 'auth', loadChildren: './auth/auth.module#AuthPageModule' },
   { path: 'scenery', loadChildren: './scenery/scenery.module#SceneryPageModule' },
-  { path: 'observe', loadChildren: './scenery/observe/observe.module#ObservePageModule' },
-  { path: 'my-scenes', loadChildren: './scenery/my-scenes/my-scenes.module#MyScenesPageModule' },
-  { path: 'new-scene', loadChildren: './scenery/my-scenes/new-scene/new-scene.module#NewScenePageModule' },
-  { path: 'edit-scene', loadChildren: './scenery/my-scenes/edit-scene/edit-scene.module#EditScenePageModule' },
-  { path: 'scene-detail', loadChildren: './scenery/observe/scene-detail/scene-detail.module#SceneDetailPageModule' },
-  { path: 'share-scenery', loadChildren: './scenery/my-scenes/share-scenery/share-scenery.module#ShareSceneryPageModule' },
-  { path: 'visits', loadChildren: './visits/visits.module#VisitsPageModule' },
+  { path: 'visits', loadChildren: './visits/visits.module#VisitsPageModule' }
 ];
 
 @NgModule({
