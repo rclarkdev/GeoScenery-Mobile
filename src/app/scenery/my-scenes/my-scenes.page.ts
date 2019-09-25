530f49453051b82062be8bf85779a1f40e4e90ca
ryan <rclarkdev1980@gmail.com>
2019-09-24 20:47:07 -0700

    my-scenes page

---
+++ src/app/scenery/my-scenes/my-scenes.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-my-scenes',
+  templateUrl: './my-scenes.page.html',
+  styleUrls: ['./my-scenes.page.scss'],
+})
+export class MyScenesPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
