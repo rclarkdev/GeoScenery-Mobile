d76ba4b87ef96ed00e43a1d60838c24f92436a94
ryan <rclarkdev1980@gmail.com>
2019-09-24 20:49:37 -0700

    share-scenery for my-scenes

---
+++ src/app/scenery/my-scenes/share-scenery/share-scenery.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-share-scenery',
+  templateUrl: './share-scenery.page.html',
+  styleUrls: ['./share-scenery.page.scss'],
+})
+export class ShareSceneryPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
